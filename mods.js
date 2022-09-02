341ae7b764cba67fd905635b5c73adab901b1878
KeinNiemand <KeinNiemand@users.noreply.github.com>
2022-09-02 15:17:34 +0200

    updated mods.js

--- mods.js
+++ mods.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Ray';
-script.src = 'https://Psycho-Ray.github.io/AutoTrimps/AutoTrimps2.js';
+script.id = 'AutoTrimps-Ray-KeinNiemandTest';
+script.src = 'https://raw.githack.com/KeinNiemand/AutoTrimpsRay/test/AutoTrimps2.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
