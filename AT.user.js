d924e0b3dcd8977fe28f103991ccb3f570b93f1f
Kein Niemand <18308201+KeinNiemand@users.noreply.github.com>
2022-09-02 23:18:47 +0200

    update readme and load scripts

--- AT.user.js
+++ AT.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         AutoTrimps-Ray
+// @name         AutoTrimps-ZekRayMergeTest
 // @version      6.0.0.0
-// @namespace    https://Psycho-Ray.github.io/AutoTrimps
-// @downloadURL  https://Psycho-Ray.github.io/AutoTrimps/.user.js
-// @updateURL    https://Psycho-Ray.github.io/AutoTrimps/.user.js
+// @namespace    https://raw.githack.com/KeinNiemand/AutoTrimps/ZekMergeTest/AutoTrimps2.js
+// @downloadURL  https://raw.githack.com/KeinNiemand/AutoTrimps/ZekMergeTest/AT.user.js
+// @updateURL    https://raw.githack.com/KeinNiemand/AutoTrimps/ZekMergeTest/AT.user.js
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Psycho-Ray, livercat
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Psycho-Ray, livercat, KeinNiemand
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
 // @connect      *Zorn192.github.io/AutoTrimps*
@@ -15,9 +15,9 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Ray';
+script.id = 'AutoTrimps-ZekRayMerge';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Psycho-Ray.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://raw.githack.com/KeinNiemand/AutoTrimps/ZekMergeTest/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
--- mods.js
+++ mods.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.id = 'AutoTrimps-ZekRayMerge';
+script.src = 'https://raw.githack.com/KeinNiemand/AutoTrimps/ZekMergeTest/AutoTrimps2.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
 
