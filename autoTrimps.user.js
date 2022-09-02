5f0cf6b4c1eda163b397c7e3393d5d73dd28ff73
KeinNiemand <KeinNiemand@users.noreply.github.com>
2022-09-02 14:17:14 +0200

    change basepath and user js so this fork get's used.

--- autoTrimps.user.js
+++ autoTrimps.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         AutoTrimps-Ray
+// @name         AutoTrimps-KeinNiemand
 // @version      6.0.0.0
-// @namespace    https://Psycho-Ray.github.io/AutoTrimps
-// @downloadURL  https://Psycho-Ray.github.io/AutoTrimps/.user.js
-// @updateURL    https://Psycho-Ray.github.io/AutoTrimps/.user.js
+// @namespace    https://raw.githack.com/KeinNiemand/AutoTrimpsRay/test/AutoTrimps
+// @downloadURL  https://raw.githack.com/KeinNiemand/AutoTrimpsRay/test/autoTrimps.user.js
+// @updateURL    https://raw.githack.com/KeinNiemand/AutoTrimpsRay/test/autoTrimps.user.js
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Psycho-Ray, livercat
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Psycho-Ray, livercat, KeinNiemand
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
 // @connect      *Zorn192.github.io/AutoTrimps*
@@ -15,9 +15,9 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Ray';
+script.id = 'AutoTrimps-KeinNiemand';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Psycho-Ray.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://raw.githack.com/KeinNiemand/AutoTrimpsRay/test/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
