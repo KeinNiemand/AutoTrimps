b543a55f9855c021c234af6915451deea169f28c
KeinNiemand <KeinNiemand@users.noreply.github.com>
2022-09-02 15:10:39 +0200

    enable buying equipment offline

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -166,11 +166,10 @@ function mainLoop() {
         const vmStatus = getVoidMapStatus();
         const hdStats = new HDStats(vmStatus);
 
-        //Offline Progress
-        if (!usingRealTimeOffline) { 
-            setScienceNeeded();
-            autoLevelEquipment(hdStats, vmStatus);
-        }
+
+        setScienceNeeded();
+        autoLevelEquipment(hdStats, vmStatus);
+
 
         //Core
         if (getPageSetting('AutoMaps') > 0) {
